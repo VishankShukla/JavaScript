78e6c093373eec793f713f9e2798e4b6afd53774
Vishank Shukla <vishankshukla51@gmail.com>
2026-02-22 01:17:53 +0530

    datatype

---
+++ Part 1/datatype.js
@@ -0,0 +1,28 @@
+/*
+String
+Number
+Boolen
+BigInt
+
+Undefined
+null
+
+object
+
+symbol
+*/
+// number
+let marks = 1234567;
+console.log(marks);
+// string
+let name = "Modi";
+console.log(name);
+// boolen
+let isLog = true
+console.log(isLog);
+// object
+let names = ["ram","shyam","mohan"];
+console.log(names)
+
+let person = {firstname : "Vishank", lastname:"Shukla"};
+console.log(person)
