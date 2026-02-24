693233a40b94ae25e1e816d6780eadbf56520907
Vishank Shukla <vishankshukla51@gmail.com>
2026-02-25 01:47:12 +0530

    non premitives

---
+++ part2/nonpremitives.js
@@ -0,0 +1,24 @@
+let username ={
+    firstName : "Vishank",
+    isLoggedIn : true,
+};
+username.lastName = "Shukla";
+console.log(username.firstName);
+console.log(username.lastName);
+console.log(username);
+
+console.log(typeof(username));
+
+let todayDate = new Date();
+console.log(todayDate.getDate());
+console.log(todayDate.getFullYear());
+
+// Array
+let fruits = ["apple","banana","mango"];
+ console.log(fruits);
+
+
+ let isLog = true;
+ console.log(Number(isLog));
+ 
+
