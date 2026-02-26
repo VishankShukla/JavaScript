a610b12822ef59c5af764d72cf9f7ec289522d04
Vishank Shukla <vishankshukla51@gmail.com>
2026-02-27 01:25:52 +0530

    while loops

---
+++ part4/loopChallenges.js
@@ -0,0 +1,17 @@
+// while loop sum of 5 number
+let i=1,sum=0;
+while(i<=5){
+    sum+=i;
+    i++;
+}
+console.log(sum);
+
+// while loop that count down 5 to 1 ; store in array named countDown
+let CountDown=[];
+let j=5;
+while(j>0){
+    CountDown.push(j);
+    j--;
+}
+console.log(CountDown);
+
