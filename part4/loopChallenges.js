eec3a65c182b68590b78b6e1fe6f9c7b0adbb85e
Vishank Shukla <vishankshukla51@gmail.com>
2026-03-01 12:13:16 +0530

    Loop Challenges

--- part4/loopChallenges.js
+++ part4/loopChallenges.js
@@ -15,3 +15,33 @@ while(j>0){
 }
 console.log(CountDown);
 
+// do while loop prompts a user to enter their favorite tea type until they enter "Stop";
+/*let teaCollection = [];
+let tea;
+do {
+    tea = prompt(`Enter your favourite tea (type "stop" to finish)`);
+    if(tea !== "stop"){
+        teaCollection.push(tea);
+    }
+    
+} while (tea !== "stop");
+console.log(teaCollection);
+*/
+// add 1 to 3 using do while loop
+let resultAdd =0;
+let k =1;
+do {
+    resultAdd+=k;
+    k++;
+} while (k<=3);
+console.log(resultAdd);
+
+// multiply the every element of the array using for loop by 2
+let newCollection =[6,3,5,3];
+let multipliedNumber = [];
+for(let it=0;it<newCollection.length;it++){
+    multipliedNumber.push(newCollection[it]*2);
+}
+console.log(newCollection);
+console.log(multipliedNumber);
+
